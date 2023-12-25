4bb91ff594ba9c68055fd6bfa2f6fc4d0da88913
neo321654 <neo321654@rambler.ru>
2023-12-25 18:28:16 +0300

    1111

--- lib/ui/widgets/example_widget.dart
+++ lib/ui/widgets/example_widget.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 import '/domain/services/auth_service.dart';
 import '/domain/services/user_service.dart';
 import '/ui/navigation/main_navigation.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class _ViewModelState {
   final String ageTitle;
+
   _ViewModelState({
     required this.ageTitle,
   });
@@ -16,6 +19,7 @@ class _ViewModel extends ChangeNotifier {
   final _userService = UserService();
 
   var _state = _ViewModelState(ageTitle: '');
+
   _ViewModelState get state => _state;
 
   void loadValue() async {
@@ -52,76 +56,75 @@ class _ViewModel extends ChangeNotifier {
   }
 }
 
-class ExampleWidget extends StatelessWidget {
-  const ExampleWidget({Key? key}) : super(key: key);
+class GreetingScreen extends StatelessWidget {
+  const GreetingScreen({Key? key}) : super(key: key);
 
   static Widget create() {
     return ChangeNotifierProvider(
       create: (_) => _ViewModel(),
-      child: const ExampleWidget(),
+      child: const GreetingScreen(),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.read<_ViewModel>();
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          ElevatedButton(
-            onPressed: () => viewModel.onLogoutPressed(context),
-            child: const Text('Выход'),
+    return CupertinoPageScaffold(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverPersistentHeader(
+            delegate: MyNav(),
+            pinned: true,
+            floating: false,
           ),
+          // SliverSafeArea(
+          //   top: false,
+          //   minimum: const EdgeInsets.only(top: 4),
+          //   sliver: SliverList(
+          //     delegate: _buildSliverChildBuilderDelegate(model),
+          //   ),
+          // )
         ],
       ),
-      body: const SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _AgeTitle(),
-              _AgeIncrementWidget(),
-              _AgeDecrementWidget(),
-            ],
+    );
+  }
+}
+
+
+
+
+
+
+class MyNav extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // TODO: implement build
+    return CupertinoNavigationBar(
+      middle: Container(
+        color: Colors.red,
+        child: const Text(
+          'Search',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 15.5,
           ),
         ),
       ),
     );
   }
-}
-
-class _AgeTitle extends StatelessWidget {
-  const _AgeTitle({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final title = context.select((_ViewModel vm) => vm.state.ageTitle);
-    return Text(title);
-  }
-}
-
-class _AgeIncrementWidget extends StatelessWidget {
-  const _AgeIncrementWidget({Key? key}) : super(key: key);
+  // TODO: implement maxExtent
+  double get maxExtent => 100.0;
 
   @override
-  Widget build(BuildContext context) {
-    final viewModel = context.read<_ViewModel>();
-    return ElevatedButton(
-      onPressed: viewModel.onIncrementButtonPressed,
-      child: const Text('+'),
-    );
-  }
-}
-
-class _AgeDecrementWidget extends StatelessWidget {
-  const _AgeDecrementWidget({Key? key}) : super(key: key);
+  // TODO: implement minExtent
+  double get minExtent => 100.0;
 
   @override
-  Widget build(BuildContext context) {
-    final viewModel = context.read<_ViewModel>();
-    return ElevatedButton(
-      onPressed: viewModel.onDecrementButtonPressed,
-      child: const Text('-'),
-    );
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    return false;
   }
 }
--- lib/ui/widgets/my_app.dart
+++ lib/ui/widgets/my_app.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         } else if (settings.name == 'example') {
           return PageRouteBuilder<dynamic>(
             pageBuilder: (context, animation1, animation2) =>
-                ExampleWidget.create(),
+                GreetingScreen.create(),
             transitionDuration: Duration.zero,
           );
         } else if (settings.name == 'loader') {
