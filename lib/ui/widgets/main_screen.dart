6b5d0a070a61023b4301a53228461fcd16305d21
neo321654 <neo321654@rambler.ru>
2023-12-28 21:36:49 +0300

    routing11

---
+++ lib/ui/widgets/main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
+import '../navigation/app_router.dart';
+
+@RoutePage()
+class MainScreenWidgetPage extends StatelessWidget {
+  const MainScreenWidgetPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      const Placeholder();
+  }
+
+
+}
