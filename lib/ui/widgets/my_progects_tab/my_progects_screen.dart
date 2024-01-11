38a30dc50ec7d1d34d25b44728a61ad85f25816f
neo321654 <neo321654@rambler.ru>
2024-01-11 22:07:42 +0300

    verstka on drunk

--- lib/ui/widgets/my_progects_tab/my_progects_screen.dart
+++ lib/ui/widgets/my_progects_tab/my_progects_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 @RoutePage()
 class MainScreenWidgetPage extends StatelessWidget {
@@ -7,9 +8,17 @@ class MainScreenWidgetPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      const Placeholder();
+    return  SafeArea(child:Column(
+      children: [
+        SizedBox(height: 8.0,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.add,size: 21.0,)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.add,size: 21.0,)),
+          ],
+        ),
+      ],
+    ));
   }
-
-
 }
