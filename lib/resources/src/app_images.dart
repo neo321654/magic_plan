697e2ab475050a55dd74a6eae54ed196e63d8f84
neo321654 <neo321654@rambler.ru>
2024-01-12 15:33:52 +0300

    verstka first

--- lib/resources/src/app_images.dart
+++ lib/resources/src/app_images.dart
@@ -3,4 +3,7 @@ abstract class AppImages {
   static const String closed_yey = 'assets/svg/closed_yey.svg';
   static const String open_yey = 'assets/svg/open_yey.svg';
   static const String doted = 'assets/svg/doted.svg';
+  static const String history_icon = 'assets/svg/history_icon.svg';
+  static const String tune_icon = 'assets/svg/tune_icon.svg';
+  static const String plus = 'assets/svg/plus.svg';
 }
--- lib/ui/widgets/my_progects_tab/my_progects_screen.dart
+++ lib/ui/widgets/my_progects_tab/my_progects_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:magic_plan/resources/resources.dart';
 
 @RoutePage()
 class MainScreenWidgetPage extends StatelessWidget {
@@ -13,25 +15,30 @@ class MainScreenWidgetPage extends StatelessWidget {
         const SizedBox(
           height: 8.0,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.add,
-                  size: 21.0,
-                )),
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.add,
-                  size: 21.0,
-                )),
-          ],
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                borderRadius: BorderRadius.circular(34),
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(AppImages.tune_icon),
+                ),
+              ),
+              InkWell(
+                borderRadius: BorderRadius.circular(34),
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(AppImages.plus),
+                ),
+              ),
+            ],
+          ),
         ),
-
-
       ],
     ));
   }
