b7154157f168a21212efb1e9a2a2fd87159eaeda
neo321654 <neo321654@rambler.ru>
2024-01-12 16:38:51 +0300

    verstka

--- lib/ui/widgets/my_progects_tab/my_progects_screen.dart
+++ lib/ui/widgets/my_progects_tab/my_progects_screen.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:magic_plan/resources/resources.dart';
@@ -38,6 +39,27 @@ class MainScreenWidgetPage extends StatelessWidget {
               ),
             ],
           ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: CupertinoSearchTextField(
+            padding: const EdgeInsets.symmetric(
+              vertical: 12.0,
+              horizontal: 8.0,
+            ),
+            itemSize: 26,
+            suffixMode: OverlayVisibilityMode.always,
+            placeholder: 'Поиск проектов'.tr,
+            suffixIcon: const Icon(
+              Icons.mic,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10.0,
         ),
       ],
     ));
